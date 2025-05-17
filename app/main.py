3dbfac624dee50f95018fc9db0a4948dd2c5d935
Yuduu <yuduu@bravecatcastle.de>
2025-05-17 11:19:54 +0200

    updated BASE_DIR

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 import os
+from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from datetime import date
 
+BASE_DIR = Path(__file__).resolve().parent
+
 # This is the fix: mount app under a proxy path
 root_path = os.getenv("ROOT_PATH", "")
-app = FastAPI(root_path=root_path)
-templates = Jinja2Templates(directory="templates")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app = FastAPI(root_path=root_path)
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
